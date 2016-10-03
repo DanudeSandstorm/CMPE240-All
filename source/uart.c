e89c91dbbf6bc1678456447644b204fe08916c83
Daniel <dds2130@rit.edu>
2016-10-03 13:35:51 -0400

    implemented functions

--- source/uart.c
+++ source/uart.c
@@ -166,27 +166,27 @@ void init_uart()
 extern char get_char()
 {
 	// <<Insert polling function here>>
-	
+	wait_for_rx_has_char();
 
 	// Do not remove!!! The RPi UART is a bit... lazy... 
     delay(150);
 	
 	// Read the data register.
     // Only care about the last 8 bits, so mask them off.
-    return (char)(uart[UART0_DR] <<APPLY BITWISE MASK HERE>>);
+    return (char)(uart[UART0_DR] & 0xFF);
 }
 
 /// Writes a single character to the uart port.
 extern void put_char(char c)
 {
 	// <<Insert polling function here>>
-	
+	wait_for_tx_slot();
 	
 	// Do not remove!!! The RPi UART is a bit... lazy... 
     delay(150);
 	
 	// <<Write the character to the data register here.>>
-	
+	uart[UART0_DR] = c;
 }
 
 /// Reads the string from uart.  It reads until either
@@ -205,14 +205,15 @@ extern size_t get_string(char* buffer, size_t buffer_size)
     )
     {
         // <<GET THE CHAR HERE>>
-		
+		ch = get_char();
 
         if ((ch != '\n') && (ch != '\r'))
         {
             // <<ECHO THE CHAR HERE>>
-            
+            put_char(ch);
 			// <<ADD TO BUFFER AND INCREMENT THE BUFFER COUNT>>.
-			
+			buffer[count] = ch;
+            count++;
         }
         // OS dependent.  May get \r, may get \n.  Either way, we'll
         // want to break out of the loop, and echo back new line (\r\n).
@@ -241,14 +242,16 @@ extern void put_string(const char* str)
     for (size_t i = 0; currentChar != '\0'; ++i)
     {
         // << SET CURRENT CHARACTER >>
-
+        currentChar = str[i];
         // Depending on which console is being used, this may
         // or may not be needed.  On linux, we've discovered we can't
         // send null characters or the formatting is all wrong.
         if(currentChar!= '\0')
         {
             // << PRINT OUT THE CURRENT CHARACTER >>
+            put_char(currentChar);
         }
     }
     // << INSERT POLLING FUNCTION CALL HERE>>
+    wait_for_uart_idle();
 }
