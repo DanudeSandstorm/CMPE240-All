26946d3a88f25ebdc732e04ba22aa39f06c0d857
Daniel <dds2130@rit.edu>
2016-11-25 16:23:14 -0500

    added addition test

--- source/main.c
+++ source/main.c
@@ -17,7 +17,7 @@ int main()
 
     testEncode();
 
-    // testMultiply();
+    testMultiply();
 
     // testAddition();
     
@@ -42,28 +42,33 @@ int testEncode() {
     return 0;
 }
 
-int testAddition() {
-    //const char* fyi[6] = {"0.0", "1.0", "0.5", "0.75", "13.125", "0.8330078125", "-13.125", "8388608.0"};
-    //const uint32_t ieee1[6] = {0x00000000, 0x3f800000, 0x3f000000, 0x41520000, 0x00000000, 0x00000000};
-    //const uint32_t ieee2[6] = {0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000};
-    
-    printf("Testing Addition\r\n");
-    printf("IEEE\t  | IEEE\t  | IEEE Result\t  | FYI Number\r\n");
-    for (uint32_t i = 0; i < 6; i++) {
-    }
-
-    return 0;
-}
-
 int testMultiply() {
-    //const char* fyi[6] = {"0.0", "1.0", "0.5", "0.75", "13.125", "0.8330078125", "-13.125", "8388608.0"};
-    //const uint32_t ieee1[6] = {0x00000000, 0x3f800000, 0x3f000000, 0x41520000, 0x00000000, 0x00000000};
-    //const uint32_t ieee2[6] = {0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000};
-    
+    const char* fyi[7] = {"0.0*1.0 = 0.0", "1.0*1.0 = 1.0", "0.5*0.5 = 0.25", 
+        "13.125*13.125 = 172.26562", "-13.125*-13.125 =172.26562",
+        "-13.125*0.8330078125 = -10.9332275390625", "8388608.0*1.0 = 8388608.0"};
+    const uint32_t ieee1[7] = {0x00000000, 0x3f800000, 0x3f000000, 0x41520000, 0xc1520000, 0xc1520000, 0x4b000000};
+    const uint32_t ieee2[7] = {0x3f800000, 0x3f800000, 0x3f000000, 0x41520000, 0xc1520000, 0x3f554000, 0x3f800000};
     
     printf("Testing Multiplication\r\n");
     printf("IEEE\t\t| IEEE\t\t| IEEE Result\t\t| FYI Number\r\n");
-    for (uint32_t i = 0; i < 6; i++) {
+    for (uint32_t i = 0; i < 7; i++) {
+        IEEE_FLT ieee = IeeeMult(ieee1[i], ieee2[i]);
+        printf("0x%08X\t \t| 0x%08X\t \t| 0x%08X\t \t| %s\r\n", ieee1[i], ieee2[i], ieee, fyi[i]);
     }
+    return 0;
+}
+
+int testAddition() {
+    const char* fyi[6] = {"0.0+0.0 = 0.0", "1.0+1.0 = 2.0", "0.5+0.5 = 1.0", "13.125+13.125 = 26.25", "-13.125+-13.125 = -26.25", "8388608.0+0.5 = 8388608.0"};
+    const uint32_t ieee1[6] = {0x00000000, 0x3f800000, 0x3f000000, 0x41520000, 0xc1520000, 0x4b000000};
+    const uint32_t ieee2[6] = {0x00000000, 0x3f800000, 0x3f000000, 0x41520000, 0xc1520000, 0x3f000000};
+    
+    printf("Testing Addition\r\n");
+    printf("IEEE\t\t| IEEE\t\t| IEEE Result\t\t| FYI Number\r\n");
+    for (uint32_t i = 0; i < 6; i++) {
+        IEEE_FLT ieee = IeeeAdd(ieee1[i], ieee2[i]);
+        printf("0x%08X\t \t| 0x%08X\t \t| 0x%08X\t \t| %s\r\n", ieee1[i], ieee2[i], ieee, fyi[i]);
+    }
+
     return 0;
 }
