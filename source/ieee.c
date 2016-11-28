3a7deab897186aeb9a7c094bc41b5f27f5eaa4d6
Daniel <dds2130@rit.edu>
2016-11-27 20:22:04 -0500

    addition works!

--- source/ieee.c
+++ source/ieee.c
@@ -93,10 +93,75 @@ IEEE_FLT IeeeMult(IEEE_FLT a, IEEE_FLT b) {
 }
 
 IEEE_FLT IeeeAdd(IEEE_FLT a, IEEE_FLT b) {
+	//Check for 0 (positive/negative)
+	if ((a & 0x7FFFFFFF) == 0 && (b & 0x7FFFFFFF) == 0x00000000) {
+		return 0x00000000;
+	}
+
 	IEEE_FLT number;
-	number = 0x0; //TODO
 
+	uint32_t exponent1 = (((a >> (31 - 8)) & 0xFF));
+	uint32_t exponent2 = (((b >> (31 - 8)) & 0xFF));
 
+	uint32_t mantissa1 = ((a & 0x007FFFFF) | 0x800000);
+	uint32_t mantissa2 = ((b & 0x007FFFFF) | 0x800000);
+
+	uint32_t shift = 1; //Used for normilization
+	//Normilize mantissia
+	if (exponent1 != exponent2) {
+		shift = 0;
+
+		while (exponent1 > exponent2) {
+			exponent2++;
+			mantissa2 = mantissa2 >> 1;
+		}
+		while (exponent2 > exponent1) {
+			exponent1++;
+			mantissa1 = mantissa1 >> 1;
+		}
+	}
+
+	uint32_t exponent = exponent1; //Since they are equal, it doesn't matter
+
+	uint32_t sign1 = ((a >> 31) & 1);
+	uint32_t sign2 = ((b >> 31) & 1);
+
+	uint32_t sign = 0;
+	uint32_t mantissa = 0;
+	//Subtraction
+	//If one sign is negative but not both
+	if ((sign1 ^ sign2) == 1) {
+
+		if (mantissa1 == mantissa2) {
+			return 0x0;
+		}
+		else if (mantissa1 > mantissa2) {
+			sign = sign1;
+			mantissa = (mantissa1 - mantissa2);
+		}
+		else if (mantissa2 > mantissa1) {
+			sign = sign2;
+			mantissa = (mantissa2 - mantissa1);
+		}
+	}
+	else {
+		//Addition
+		mantissa = mantissa1 + mantissa2;
+
+		//If both negative
+		if (sign1 == 1 && sign2 == 1) {
+			sign = 0x1;
+		}
+	}
+
+	//Re-normilize
+	mantissa = mantissa >> shift;
+	exponent += shift;
+	//Remove implied 1
+	mantissa = (mantissa & 0x007FFFFF);
+
+	
+	number = (sign << 31) | (exponent << (31 - 8)) | mantissa;
 
 	//Check for 0 (negative)
 	if (number == 0x80000000) {
